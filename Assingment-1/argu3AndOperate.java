311c3b892a2be2c58dc3970c0057aa70ec8c0800
vidyabhandari <vidyabhandari2804@gmail.com>
2025-04-17 22:32:47 +0530

    input of 3 arguments and operate program added

---
+++ Assingment-1/argu3AndOperate.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class argu3AndOperate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second numer: ");
+        int num2 = sc.nextInt();
+
+        System.err.print("Enter the Operator (+,-,*,/): ");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid input!");
+                break;
+        }
+    }
+}
