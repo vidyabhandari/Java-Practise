9a7d5010ac7e110890e93b9dd731e8180907fa14
vidyabhandari <vidyabhandari2804@gmail.com>
2025-04-18 11:13:28 +0530

    upper to lower and vice versa program added

---
+++ Assingment-1/upperToLowerViceVersa.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class upperToLowerViceVersa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentance: ");
+        String sen = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < sen.length(); i++) {
+            char ch = sen.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                result = result + Character.toLowerCase(ch);
+            } else if (Character.isLowerCase(ch)) {
+                result = result + Character.toUpperCase(ch);
+            } else {
+                result = result + ch;
+            }
+        }
+        System.out.print("Changed Sentence: " + result);
+    }
+}
